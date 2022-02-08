ecffcfa9d97e3ad5fad30732913fcf133aa1b670
Valentin Mendoza <jmendozat13@gmail.com>
2022-02-07 21:41:51 -0500

    Updated version

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -5,5 +5,5 @@ object ConfigureApp {
     const val applicationId = "app-android-orders"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "orders"
-    const val version = "1.0.0"
+    const val version = "1.0.1"
 }
