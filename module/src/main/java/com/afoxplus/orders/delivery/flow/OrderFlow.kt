bcdd9306c0c2fb1142ffff6edbe70d277af49bea
Petter Garcia <petter.infante@macys.com>
2022-07-04 15:58:56 -0500

    add success order screen

--- module/src/main/java/com/afoxplus/orders/delivery/flow/OrderFlow.kt
+++ module/src/main/java/com/afoxplus/orders/delivery/flow/OrderFlow.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import com.afoxplus.orders.delivery.views.activities.AddProductToOrderActivity
 import com.afoxplus.orders.delivery.views.activities.OrderPreviewActivity
 import com.afoxplus.orders.delivery.views.activities.MarketOrderActivity
+import com.afoxplus.orders.delivery.views.activities.OrderSuccessActivity
 import com.afoxplus.orders.entities.Order
 import com.afoxplus.products.entities.Product
 import javax.inject.Inject
@@ -14,6 +15,7 @@ interface OrderFlow {
     fun goToMarketOrderActivity(activity: Activity)
     fun goToAddProductToOrderActivity(activity: Activity, product: Product)
     fun goToOrderPreviewActivity(activity: Activity, order: Order)
+    fun goToOrderSuccessActivity(activity: Activity)
 
     class OrderFlowImpl @Inject constructor() : OrderFlow {
         override fun goToMarketOrderActivity(activity: Activity) {
@@ -34,6 +36,10 @@ interface OrderFlow {
                     putParcelable(Order::class.java.name, order)
                 })
             })
+        }
+
+        override fun goToOrderSuccessActivity(activity: Activity) {
+            activity.startActivity(Intent(activity, OrderSuccessActivity::class.java))
         }
 
     }
