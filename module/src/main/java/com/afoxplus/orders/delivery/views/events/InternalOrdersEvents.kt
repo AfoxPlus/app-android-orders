ace5c5449d2e7d700be83c1b7add2bf8ab867fc6
Valentin Mendoza <jmendozat13@gmail.com>
2023-08-03 20:05:49 -0500

    Update scope events

--- module/src/main/java/com/afoxplus/orders/delivery/views/events/InternalOrdersEvents.kt
+++
@@ -1,6 +0,0 @@
-package com.afoxplus.orders.delivery.views.events
-
-import com.afoxplus.uikit.bus.UIKitEventBus
-
-internal val AddedProductToCurrentOrderSuccessfullyEvent = object : UIKitEventBus {}
-internal val GoToNewOrderEvent = object : UIKitEventBus {}
