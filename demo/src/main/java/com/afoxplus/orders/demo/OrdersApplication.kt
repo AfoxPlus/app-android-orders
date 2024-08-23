c576e255199db843cff90179713d8ddc0941f904
Valentin Mendoza Torres <jmendozat13@gmail.com>
2024-08-23 16:35:54 -0500

    Update gradle (#61)
    * Update gradle
    
    * Update dependencies

--- demo/src/main/java/com/afoxplus/orders/demo/OrdersApplication.kt
+++ demo/src/main/java/com/afoxplus/orders/demo/OrdersApplication.kt
@@ -18,12 +18,11 @@ class OrdersApplication : Application() {
         vendorShared.save(
             Vendor(
                 tableId = "-",
-                restaurantId = "66b924a5b835aa5b0e421289",
+                restaurantId = "648f94bd704db9741d1d2c04",
                 additionalInfo = mutableMapOf(
                     "restaurant_name" to "Rest. Pepito sazon!",
                     "restaurant_own_delivery" to false,
-                    "restaurant_order_type" to "TABLE",
-                    "invitation_guest" to "Invitation guest name"
+                    "restaurant_order_type" to "DELIVERY"
                 ),
                 paymentMethod = arrayListOf(
                     PaymentMethod("12", "Cúpon de Invitación", true),
